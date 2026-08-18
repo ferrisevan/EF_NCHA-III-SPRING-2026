646d2a8a8152fe306490c8998f33ea83cb506bec
Evan Ferris <ferrisevan1@gmail.com>
2026-08-18 12:26:48 -0400

    creation of a prelim cleanup and analysis script along with a new excel file that excludes NAs, school, start date, and end date.

---
+++ NCHA-III_prelim_analysis.R
@@ -0,0 +1,21 @@
+##read in the NCHA-III excel file
+library(readxl)
+ef_NCHA_III_WEB_SPRING_2026_UNIVERSITY_OF_TENNESSEE_KNOXVILLE1 <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx")
+View(ef_NCHA_III_WEB_SPRING_2026_UNIVERSITY_OF_TENNESSEE_KNOXVILLE1)
+
+##remove the NAs from the file
+library(openxlsx)
+df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx",
+                 sheet = "NCHA-III WEB SPRING 2026 UNIVER")
+df[] <- lapply(df, as.character)
+df[is.na(df)] <- ""
+write.xlsx(df, "NCHA-III_no_NAs.xlsx")
+
+##next, remove the school, start date, and end date columns (useless metadata that does not provide us with any insight)
+df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx",
+                 sheet = "NCHA-III WEB SPRING 2026 UNIVER")
+df <- df[ , !(names(df) %in% c("School", "StartDate", "EndDate"))]
+df[] <- lapply(df, as.character)
+df[is.na(df)] <- ""
+write.xlsx(df, "NCHA-III_tidy.xlsx")
+View(df)
