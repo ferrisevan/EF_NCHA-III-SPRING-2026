36a5517634f1e4c1140881b0638944d8b4566106
Evan Ferris <ferrisevan1@gmail.com>
2026-08-18 12:34:53 -0400

    rewrote code to fix an issue that created two separate cleaned versions of the excel file.

--- NCHA-III_prelim_analysis.R
+++ NCHA-III_prelim_analysis.R
@@ -9,11 +9,9 @@ df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1
                  sheet = "NCHA-III WEB SPRING 2026 UNIVER")
 df[] <- lapply(df, as.character)
 df[is.na(df)] <- ""
-write.xlsx(df, "NCHA-III_tidy.xlsx")
+
 
 ##next, remove the school, start date, and end date columns (useless metadata that does not provide us with any insight)
-df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx",
-                 sheet = "NCHA-III WEB SPRING 2026 UNIVER")
 df <- df[ , !(names(df) %in% c("School", "StartDate", "EndDate"))]
 df[] <- lapply(df, as.character)
 df[is.na(df)] <- ""
