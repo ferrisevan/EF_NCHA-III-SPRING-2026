11bf82da8054a6ba419b4a4cf6bbc403f4b01d25
Evan Ferris <ferrisevan1@gmail.com>
2026-08-18 13:34:48 -0400

    reworked the script by consolidating functions. will see if this produces the output I need for the excel file.

--- NCHA-III_prelim_analysis.R
+++ NCHA-III_prelim_analysis.R
@@ -1,19 +1,22 @@
 ##read in the NCHA-III excel file
 library(readxl)
-ef_NCHA_III_WEB_SPRING_2026_UNIVERSITY_OF_TENNESSEE_KNOXVILLE1 <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx")
-View(ef_NCHA_III_WEB_SPRING_2026_UNIVERSITY_OF_TENNESSEE_KNOXVILLE1)
+library(writexl)
+library(tidyverse)
 
-##remove the NAs from the file
-library(openxlsx)
-df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx",
+df <- read_excel("ef_NCHA-III WEB SPRING 2026 UNIVERSITY OF TENNESSEE KNOXVILLE1.xlsx", 
                  sheet = "NCHA-III WEB SPRING 2026 UNIVER")
-df[] <- lapply(df, as.character)
-df[is.na(df)] <- ""
 
-
-##next, remove the school, start date, and end date columns (useless metadata that does not provide us with any insight)
+##next, remove the school, start date, and end date columns (metadata that does not provide us with any useful insight)
 df <- df[ , !(names(df) %in% c("School", "StartDate", "EndDate"))]
-df[] <- lapply(df, as.character)
-df[is.na(df)] <- ""
-write.xlsx(df, "NCHA-III_tidy.xlsx")
-View(df)
+
+##convert the numbers from characters back into numerics and remove the NAs from the file
+df_clean <- df %>%
+  mutate(
+    across(everything(), ~ {
+      cleaned <- gsub("[$, ]", "", .)
+      num_val <- as.numeric(cleaned)
+      as.character(if_else(is.na(num_val), "", as.character(num_val)))
+    })
+  )
+
+write_xlsx(df_clean, "NCHA-III_tidy.xlsx")
